3724158dee830943969e3d6006ede9a73d89a17d
jabir11757 <smjalsaba11757@gmail.com>
2023-01-27 09:56:56 +0600

    Revert "File name & code changed"
    This reverts commit b889dcd7802ad2c2e222602a3149812bfc715205.

--- src/Test/SinglePage/Modals/Modals.js
+++ src/Test/SinglePage/Modals/Modals.js
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useState } from 'react';
 
-export default function Modal({ showModal, setShowModal }) {
-
+const Modals = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [data, setData] = useState('');
     return (
         <>
-
+            <button
+                className="bg-green-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => setShowModal(true)}
+            >
+                Open regular modal
+            </button>
             {showModal ? (
-                <div>
+                <>
                     <div
                         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
                     >
-                        <div className="relative h-full">
+                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
                             {/*content*/}
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 {/*header*/}
                                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                                     <h3 className="text-3xl font-semibold">
-                                        All Photos
+                                        Modal Title
                                     </h3>
                                     <button
                                         className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
@@ -28,10 +35,14 @@ export default function Modal({ showModal, setShowModal }) {
                                     </button>
                                 </div>
                                 {/*body*/}
-                                <div className=" p-6 w-3/4 mx-auto border">
-                                    <div>
-                                        <img style={{ width: "100vw" }} src="https://media.istockphoto.com/id/1368264124/photo/extreme-close-up-of-thrashing-emerald-ocean-waves.jpg?b=1&s=170667a&w=0&k=20&c=qha_PaU54cu9QCu1UTlORP4-sW0MqLGERkdFKmC06lI=" alt="" />
-                                    </div>
+                                <div className="relative p-6 flex-auto">
+                                    <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                                        I always felt like I could do anything. That’s the main
+                                        thing people are controlled by! Thoughts- their perception
+                                        of themselves! They're slowed down by their perception of
+                                        themselves. If you're taught you can’t do anything, you
+                                        won’t do anything. I was taught I could do everything.
+                                    </p>
                                 </div>
                                 {/*footer*/}
                                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
@@ -42,13 +53,22 @@ export default function Modal({ showModal, setShowModal }) {
                                     >
                                         Close
                                     </button>
+                                    <button
+                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Save Changes
+                                    </button>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-                </div>
+                </>
             ) : null}
         </>
     );
-}
+};
+
+export default Modals;
--- src/Test/SinglePage/SinglePage.js
+++ src/Test/SinglePage/SinglePage.js
@@ -1,38 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FaIndent } from 'react-icons/fa';
 import DetailSection from './DetailsSection/DetailSection';
-import Modals from './Modals/Modals';
 
 const SinglePage = () => {
-
-    const [showModal, setShowModal] = useState(false);
-
-
     return (
         <div>
-            <div className='flex p-2' style={{ width: "100%", height: "100%" }}>
-                <div style={{ width: "50%", height: "100%" }}>
+            <div className='flex p-2' style={{ width: "100%" }}>
+                <div style={{ width: "50%" }}>
                     <img className='w-full px-4' src='https://media.istockphoto.com/id/1368264124/photo/extreme-close-up-of-thrashing-emerald-ocean-waves.jpg?b=1&s=170667a&w=0&k=20&c=qha_PaU54cu9QCu1UTlORP4-sW0MqLGERkdFKmC06lI=' alt='' />
                 </div>
                 <div style={{ width: "50%", height: "100%" }}>
                     <div className='grid gap-4 grid-cols-2'>
 
-                        <div className=''>
+                        <div className='h-50'>
                             <img className='h-full w-full' src='https://media.istockphoto.com/id/1368264124/photo/extreme-close-up-of-thrashing-emerald-ocean-waves.jpg?b=1&s=170667a&w=0&k=20&c=qha_PaU54cu9QCu1UTlORP4-sW0MqLGERkdFKmC06lI=' alt='' />
                         </div>
-                        <div className=''>
+                        <div className='h-50'>
                             <img className='h-full w-full' src='https://live.staticflickr.com/65535/52530446584_cb1dce6453_z.jpg' alt='' />
                         </div>
 
 
-                        <div className=''>
+                        <div className='h-50'>
                             <img className='h-full w-full' src='https://live.staticflickr.com/65535/52530446584_cb1dce6453_z.jpg' alt='' />
                         </div>
-                        <div className=' relative'>
+                        <div className='h-50 relative'>
                             <img className='h-full w-full' src='https://live.staticflickr.com/65535/52530446584_cb1dce6453_z.jpg' alt='' />
                             <div className='flex absolute bg-white hover:bg-slate-200 mx-2' style={{ width: "12rem", marginTop: "-7vh", padding: ".4rem" }}>
                                 <FaIndent className='mt-1 mx-2' />
-                                <button onClick={() => setShowModal(true)}>show all photos</button>
+                                <button>show all photos</button>
                             </div>
                         </div>
 
@@ -41,10 +36,6 @@ const SinglePage = () => {
             </div >
             <div>
                 <DetailSection />
-            </div>
-
-            <div>
-                <Modals setShowModal={setShowModal} showModal={showModal} />
             </div>
         </div>
     );
